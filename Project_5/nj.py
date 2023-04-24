9e5188071cac73ca35b47eb4d8e1a9b0df7bee28
Rasmus Freund <rasmus3141@gmail.com>
2023-04-24 21:11:16 +0200

    Update

--- Project_5/nj.py
+++ Project_5/nj.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 import phy_parser
 import numpy as np
 
@@ -9,58 +10,77 @@ parser.add_argument("file", help = "File should be a phylip-format distance-matr
 args = parser.parse_args()
 
 
-def neighbour_joining(dist_matrix):
-    # Convert distance matrix to numpy array
-    D = np.array(dist_matrix)
-    # Create a dictionary to hold the labels of the nodes in the tree
-    node_labels = {i: str(i) for i in range(len(D))}
-    # Create a dictionary to hold the distances between nodes in the tree
-    node_distances = {}
+class NeighborJoining:
+    def __init__(self, disMatrix, n):
+        adj = self.runNeighborJoining(disMatrix, n)
+        self.printGraph(adj)
+        self.saveResult(adj)
 
-    # Keep iterating until we've reduced the matrix to a single element
-    while len(D) > 1:
-        # Calculate the Q matrix
-        n = len(D)
-        Q = np.zeros((n, n))
-        for i in range(n):
-            for j in range(i+1, n):
-                total_distance_i = np.sum(D[i])
-                total_distance_j = np.sum(D[j])
-                Q[i,j] = (n-2)*D[i,j] - total_distance_i - total_distance_j
-        # Find the minimum value in the Q matrix
-        min_index = np.argmin(Q)
-        i, j = np.unravel_index(min_index, Q.shape)
-        delta = (np.sum(D[i]) - np.sum(D[j])) / (n - 2)
-        # Calculate the new distance between the nodes
-        new_distance = (D[i,j] + delta, D[i,j] - delta)
-        # Create a new node to represent the cluster
-        new_label = "(" + node_labels[i] + ":" + str(new_distance[0]) + "," + node_labels[j] + ":" + str(new_distance[1]) + ")"
-        node_labels[n] = new_label
-        node_distances[new_label] = new_distance
-        # Update the distance matrix
-        new_row = np.zeros(n-1)
-        new_D = np.zeros((n-1, n-1))
-        for k in range(n):
-            if k == i or k == j:
-                continue
-            new_row[k if k < j else k-1] = (D[i,k] + D[j,k] - D[i,j]) / 2
-            for l in range(n):
-                if l == i or l == j:
-                    continue
-                new_D[k if k < j else k-1, l if l < j else l-1] = D[k,l]
-        new_D[-1] = new_row
-        new_D[:,-1] = new_row
-        D = new_D
+    def saveResult(self, adj):
+        f = open('result.txt', 'w')
+        for i, nodes in enumerate(adj):
+            for d, w in nodes:
+                f.write(str(i)+'->'+str(d)+':'+'%0.3f' % w+'\n')
 
-    # Return the final tree in Newick format
-    return node_labels[0] + ";"
+    def printDistMatrix(self, distMatrix):
+        for d in distMatrix:
+            print(' '.join([str(i) for i in d]))
 
+    def printGraph(self, adj):
+        for i, nodes in enumerate(adj):
+            for d, w in nodes:
+                print(str(i)+'->'+str(d)+':'+'%0.3f' % w)
 
-def main():
+    def runNeighborJoining(self, disMatrix, n):
+        D = np.array(disMatrix, dtype = float)
+        clusters = [i for i in range(n)]
+        adj = [[] for i in range(n)]
+        if len(D) <= 1:
+            return adj
+        while True:
+            if 2 == n:
+                adj[len(adj)-1].append((len(adj)-2, D[0][1]))
+                adj[len(adj)-2].append((len(adj)-1, D[0][1]))
+                break
+            totalDist = np.sum(D, axis = 0)
+            D1 = (n-2) * D
+            D1 = D1 - totalDist
+            D1 = D1 - totalDist.reshape((n, 1))
+            np.fill_diagonal(D1, 0.)
 
-    dist_matrix = phy_parser.parser(args.file)
-    print(dist_matrix)
+            index = np.argmin(D1)
+            i = index // n
+            j = index % n
+            delta = (totalDist[i] - totalDist[j])/(n-2)
+            li = (D[i, j]+delta)/2
+            lj = (D[i, j]-delta)/2
+            d_new = (D[i, :]+D[j, :]-D[i, j])/2
+            D = np.insert(D, n, d_new, axis = 0)
+            d_new = np.insert(d_new, n, 0., axis = 0)
+            D = np.insert(D, n, d_new, axis = 1)
+            D = np.delete(D, [i, j], 0)
+            D = np.delete(D, [i, j], 1)
 
+            m = len(adj)
+            adj.append([])
+            adj[m].append((clusters[i], li))
+            adj[clusters[i]].append((m, li))
+            adj[m].append((clusters[j], lj))
+            adj[clusters[j]].append((m, lj))
+            if i < j:
+                del clusters[j]
+                del clusters[i]
+            else:
+                del clusters[i]
+                del clusters[j]
+            clusters.append(m)
+
+            n -= 1
+
+        return adj
 
 if __name__ == "__main__":
-    main()
+    disMatrix = phy_parser.parser(args.file)
+    n = len(disMatrix)
+
+    NeighborJoining(disMatrix, n)
